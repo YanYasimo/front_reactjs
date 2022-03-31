9b2968f8d8047243350bf70625a401b512c4ad08
YanYasimo <yanmoura142529@gmail.com>
2022-03-31 10:29:13 -0300

    Implementation of login, register and api

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate  } from 'react-router-dom'
 import { FiKey } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Login() {
+    
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const history = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try{
+            console.log("try");
+            const response = await api.post('/sessions', {email, password});
+
+            console.log("response", response.data);
+
+            localStorage.setItem('userId', response.data.id);
+            localStorage.setItem('userName', response.data.name);
+
+            console.log("profile");
+            // history('/profile');
+        } catch (err) {
+            alert('Falha no login, tente novamente');
+        }
+    };
+
+
     return (
         <div className="login-container">
             <section className="form">
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Fazer Login</h1>
 
-                    <input placeholder="Seu E-mail" />
-                    <input type="password" placeholder="Sua Senha" />
+                    <input 
+                        placeholder="Seu E-mail"
+                        value={email}
+                        onChange={e=> setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Sua Senha"
+                        value={password}
+                        onChange={e=> setPassword(e.target.value)}
+                    />
                     <button className="button" type="submit">Acessar</button>
 
                     <Link className="linked" to="/register">
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate  } from 'react-router-dom';
 import { FiChevronsLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api';
+
 export default function Register(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    const history = useNavigate();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        try{
+            if (password !== confirmPassword) {
+                throw new Error('Senhas não conferem');
+            }
+            const response = await api.post('/users', { name, email, password });
+            const { id } = response.data;
+
+            alert('Usuário cadastrado com sucesso!');
+
+            history('/');
+        } catch (err) {
+            console.log(err);
+            if (err.response) {
+                alert('Fail to register, try again.\n' + err.response.data.error);
+            } else {
+                alert('Fail to register, try again.');
+            }
+        }
+    };
+
     return(
         <div className="register-container">
             <div className="content">
@@ -18,13 +50,32 @@ export default function Register(){
                     </Link>
                 </section>
 
-                 <form>
-                    <input placeholder="Nome" />
-                    <input type="email" placeholder="E-mail" />
-                    <input type="password" placeholder="Senha" />
-                    <input type="password" placeholder="Confirme a Senha" />
+                 <form onSubmit={handleRegister} >
+                    <input 
+                        placeholder="Nome" 
+                        value={name}
+                        onChange={e=> setName(e.target.value)}
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="E-mail" 
+                        value={email}
+                        onChange={e=> setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Senha" 
+                        value={password}
+                        onChange={e=> setPassword(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Confirme a Senha" 
+                        value={confirmPassword}
+                        onChange={e=> setConfirmPassword(e.target.value)}
+                    />
 
-                    <button className="button">Cadastrar</button>
+                    <button className="button" type="submit">Cadastrar</button>
                  </form>
             </div>
         </div>
