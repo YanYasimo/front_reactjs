225e0a8fc2e7f259d3c4f2163f5af9820b60dc78
YanYasimo <yanmoura142529@gmail.com>
2022-04-01 20:48:58 -0300

    Feature: Timer Component

---
+++ src/components/Timer/index.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+import './styles.css';
+
+export default function Timer() {
+    return (
+        <div className="timer-container">
+            <h1>Timer</h1>
+        </div>
+    );
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Link, useNavigate  } from 'react-router-dom';
 
 import Header from '../../components/Header';
+import Timer from '../../components/Timer';
 
 import api from '../../services/api';
 
@@ -11,6 +12,7 @@ export default function Dashboard() {
     return (
         <div className="dashboard-container">
             <Header />
+            <Timer />
         </div>
     );
 };
